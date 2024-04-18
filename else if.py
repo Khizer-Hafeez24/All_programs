057660b495635d7ba0f26252c45037d73ba8d8c6
Khizer Hafeez Durrani <162121261+Khizer-Hafeez24@users.noreply.github.com>
2024-04-18 08:18:39 +0500

    Update else if.py

---
+++ else if.py
@@ -0,0 +1,10 @@
+# Get user input
+num = int(input("Enter a number: "))
+
+# Check conditions using if-elif-else
+if num > 0:
+    print("The number is positive")
+elif num < 0:
+    print("The number is negative")
+else:
+    print("The number is zero")
